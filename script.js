c57ecc3816d40f275cb0f6e60db440d98cffe26c
Dejuan2003 <43796152+Dejuan2003@users.noreply.github.com>
2019-04-09 17:12:14 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+$(".Jotaro").click(function() {
+    $(".Joseph").hide();
+});
+$(".Jotaro").dblclick(function() {
+    $(".Johnathan").hide();
+});
+$(".Jotaro").mouseenter(function() {
+    $(".Josuke").hide();
+});
+$(".Dios").hover(function() {
+    $(".Gio").hide();
+});
+$(".Jotaro").dblclick(function() {
+    $(".Jotaro").click(function() {
+        $(".Dio").hide();
+    });
+});
+$(".Dios").dblclick(function() {
+    $("p").text("I got to get closer if I want to beat the hell out of you");
+});
+$(".Dios").dblclick(function() {
+     $(".Dios").click(function() {
+    $("p").text("I got to get closer if I want to beat the hell out of you");
+        $("h2").text("OH HO!");
+   });
+});
